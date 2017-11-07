b016f6d2938f234fe844260b814be8158e1b9c1b
Yaniv Kalsky <yanivka@gmail.com>
2017-11-07 14:51:09 -0800

    moved folders

--- prepare_package.cmd
+++ prepare_package.cmd
@@ -4,19 +4,19 @@ echo pack shells
 cd Shells
 cd putshell
 shellfoundry pack
-copy dist\putshell.zip "..\..\Admin Blueprint\Scripts\Admin Setup Script\putshell.zip" /y
+copy dist\putshell.zip "..\..\Blueprints\Admin Blueprint\Scripts\Admin Setup Script\putshell.zip" /y
 cd ..
 cd trafficshell
 shellfoundry pack
-copy dist\trafficshell.zip "..\..\Admin Blueprint\Scripts\Admin Setup Script\trafficshell.zip" /y
+copy dist\trafficshell.zip "..\..\Blueprints\Admin Blueprint\Scripts\Admin Setup Script\trafficshell.zip" /y
 cd ..
 cd l2mockswitch
 shellfoundry pack
-copy dist\l2mockswitch.zip "..\..\Admin Blueprint\Scripts\Admin Setup Script\l2mockswitch.zip" /y
+copy dist\l2mockswitch.zip "..\..\Blueprints\Admin Blueprint\Scripts\Admin Setup Script\l2mockswitch.zip" /y
 cd ..\..
 
 echo pack put traffic blueprint
-cd "PUT Traffic Test Blueprint\Scripts"
+cd "Blueprints\PUT Traffic Test Blueprint\Scripts"
 cd Setup
 del *.zip
 "c:\Program Files\7-Zip\7z.exe" a "..\..\Package\Topology Scripts\Setup Training.zip" *
@@ -27,21 +27,21 @@ del *.zip
 cd..
 cd "Run Tests"
 copy "Run Tests.py" "..\..\Package\Topology Scripts" /y
-cd..\..\..
-cd "PUT Traffic Test Blueprint\Package"
+cd..\..\..\..
+cd "Blueprints\PUT Traffic Test Blueprint\Package"
 del *.zip
-"c:\Program Files\7-Zip\7z.exe" a "..\..\Admin Blueprint\Scripts\Admin Setup Script\PUT Traffic Test Blueprint.zip" *
-cd..\..
+"c:\Program Files\7-Zip\7z.exe" a "..\..\Blueprints\Admin Blueprint\Scripts\Admin Setup Script\PUT Traffic Test Blueprint.zip" *
+cd..\..\..
 
 echo pack admin setup script
-cd "Admin Blueprint\Scripts\Admin Setup Script"
+cd "Blueprints\Admin Blueprint\Scripts\Admin Setup Script"
 del "..\..\Package\topology scripts\Admin Setup Script.zip"
 "c:\Program Files\7-Zip\7z.exe" a "..\..\Package\topology scripts\Admin Setup Script.zip" *
-cd..\..\..
+cd..\..\..\..
 
 echo pack admin blueprint
-cd "Admin Blueprint\Package"
+cd "Blueprints\Admin Blueprint\Package"
 del *.zip
 "c:\Program Files\7-Zip\7z.exe" a "..\..\Packages\Admin Blueprint.zip" *
-cd..\..
+cd..\..\..
 
